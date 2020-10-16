8553e19d0c1df5dd96936f82312f0ef37b5c5032
Jose Terrera <joseterrera@yahoo.com>
2020-10-16 00:47:22 -0700

    adding routes

---
+++ routes/items.js
@@ -0,0 +1,12 @@
+const Item = require('../item');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('', (req, res, next) {
+  try {
+    return res.json({ items: Item.findAll() });
+  } catch(err) {
+    return next(err)
+  }
+})
